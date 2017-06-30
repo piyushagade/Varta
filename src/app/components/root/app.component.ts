dac2a7eb352e67d2d6722908fc03e6c25af4d1c8
Piyush Agade <piyushagade@gmail.com>
2017-06-29 22:59:48 -0400

    scroll to top on route change added

--- src/app/components/root/app.component.ts
+++ src/app/components/root/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  onActivate(e, outlet){
+    outlet.scrollTop = 0;
+  }
+
+  constructor(private router: Router){
+  }
+
+  onDeactivate() {
+    document.body.scrollTop = 0;
+}
 }
--- src/app/routers/app.router.ts
+++ src/app/routers/app.router.ts
@@ -5,8 +5,8 @@ import { BlogsComponent } from '../components/blogs/blogs.component';
 import { ArticleComponent } from '../components/article/article.component';
 
 export const router: Routes = [
-    { path: '', redirectTo: 'blogs', pathMatch: 'full' },
-    { path: 'blogs', component: BlogsComponent },
+    { path: 'blogs', redirectTo: '', pathMatch: 'full' },
+    { path: '', component: BlogsComponent },
     { path: '**', component: ArticleComponent },
 ];
 
